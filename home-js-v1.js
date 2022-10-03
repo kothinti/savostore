621940d2065738a435786855d3613bd21a805cf1
Ashok Kumar Kothinti <ashok.kothinti@gmail.com>
2022-10-03 14:27:48 +0530

    Update home-js-v1.js

--- home-js-v1.js
+++ home-js-v1.js
@@ -1,8 +1,3 @@
-<script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
-<script type="text/javascript">
-
-var app=document.getElementById("dynamic-text"),typewriter=new Typewriter(app,{loop:!0});typewriter.typeString("Mombasa").pauseFor(2500).deleteAll().typeString("Nanyuki").pauseFor(2500).deleteAll().typeString("Nairobi").pauseFor(2500).deleteAll().typeString("Kisumu").pauseFor(2500).deleteAll().typeString("Kitale").pauseFor(2500).deleteAll().typeString("Nyeri").pauseFor(2500).start();
-  
   $( document ).ready(function() {
       var rateperkg=15,totalcost,td_cost,intcust,shipprotection=0;
       var td_costv1,intcustv1,shipprotectionv1=0;
@@ -160,9 +155,3 @@ var app=document.getElementById("dynamic-text"),typewriter=new Typewriter(app,{l
         
       });
   });
-</script>
-<script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
-<script type="text/javascript">$(document).ready(function(){const r=new Intl.NumberFormat("en-US",{minimumFractionDigits:2,maximumFractionDigits:2});$("#items-dropdown").on("change",function(){var t=Number($("#item-cost-input").val()),m=10*t/100,o=Number($("#items-dropdown").val()),s=16*(m+o)/100,i=t+m+o+s,a=109.32*i;$("#item-cost").html("$"+r.format(t)),$("#service-cost").html("$"+r.format(m)),$("#shipping-cost").html("$"+r.format(o)),t&&0!=m&&($("#vat").html("$"+r.format(s)),$("#total-cost-usd").html("$ "+r.format(i)),$("#total-cost-ksh").html("Ksh "+r.format(a))),0==t&&$("#item-cost").html("$ 0"),0==m&&$("#service-cost").html("$ 0"),0==t&&0==m&&$("#vat").html("$ 0")}),$("#item-cost-input").on("input",function(){var t=Number($("#item-cost-input").val()),m=10*t/100,o=Number($("#items-dropdown").val()),s=16*(m+o)/100,i=t+m+o+s,a=109.32*i;$("#item-cost").html("$"+r.format(t)),$("#service-cost").html("$"+r.format(m)),$("#shipping-cost").html("$"+r.format(o)),t&&0!=m&&($("#vat").html("$"+r.format(s)),$("#total-cost-usd").html("$ "+r.format(i)),$("#total-cost-ksh").html("Ksh "+r.format(a))),0==t&&$("#item-cost").html("$ 0"),0==m&&$("#service-cost").html("$ 0"),0==t&&0==m&&$("#vat").html("$ 0")})});</script>
-<script>let registration_url;$(".btn-get-started-hero").click(function(e){e.preventDefault();e=new RegExp("^[a-zA-Z ]+$");$(".f-fullname-hero,.f-emailid-hero").focus(function(){$(".fname-error-hero,.email-error-hero").addClass("hide")}),0==$(".f-fullname-hero").val().length&&0==$(".f-emailid-hero").val().length?$(".fname-error-hero,.email-error-hero").removeClass("hide"):e.test($(".f-fullname-hero").val())&&0!=$(".f-fullname-hero").val().length?/^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test($(".f-emailid-hero").val())&&0!=$(".f-emailid-hero").val().length?(registration_url=encodeURI("https://app.savostore.com/#/register?first_name="+$(".f-fullname-hero").val()+"&email_address="+$(".f-emailid-hero").val()),$(".form-start-shipping").submit()):$(".email-error-hero").removeClass("hide"):$(".fname-error-hero").removeClass("hide")}),$(".btn-form-finish").click(function(e){window.open(registration_url,"_self")});</script>
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-<script>var swiper=new Swiper(".quick-price",{centeredSlides:!0,slidesPerView:1,spaceBetween:15,mousewheel:!1,keyboard:!1,loop:!0,freeMode:!1,navigation:{nextEl:".hero-swiper-arrow-next",prevEl:".hero-swiper-arrow-prev"},breakpoints:{768:{slidesPerView:1,spaceBetween:30},1024:{slidesPerView:1,spaceBetween:40,freeMode:!0,mousewheel:!0,keyboard:!0}},on:{slideChange:function(){$("#items-dropdown-hero option[data-id='"+this.realIndex+"']").prop("selected",!0)}}});$("#items-dropdown-hero").on("change",function(){let e=$(this).find("option:selected");var r=e.data("id");swiper.slideTo(r+1)});</script>
