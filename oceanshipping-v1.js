219782f3d952fb20a30eea90a97d38c13b9c1bf6
Ashok Kumar Kothinti <ashok.kothinti@gmail.com>
2023-03-17 15:58:26 +0530

    Create oceanshipping-v1.js

---
+++ oceanshipping-v1.js
@@ -0,0 +1,176 @@
+$( document ).ready(function() {
+      var rateperkg=30,totalcost,td_cost,intcust,shipprotection=0;
+      var td_costv1,intcustv1,shipprotectionv1=0,homedelivery=0,homedeliveryv1=0;
+      var cm_divider = 61023.80;
+      var cf_divider = 1728;
+
+      const formatter = new Intl.NumberFormat('en-US', {
+         minimumFractionDigits: 2,      
+         maximumFractionDigits: 2,
+      });
+	  
+      $('.cost-calc input').on('input', function() {
+	
+	$('#dimensions-length').val() ? $('#dimensions-length').css({'border-color': '#737373'}) : $('#dimensions-length').css({'border-color': 'red'});
+      	$('#dimensions-width').val() ? $('#dimensions-width').css({'border-color': '#737373'}) : $('#dimensions-width').css({'border-color': 'red'});
+      	$('#dimensions-height').val() ? $('#dimensions-height').css({'border-color': '#737373'}) : $('#dimensions-height').css({'border-color': 'red'});
+				
+        if($('#dimensions-length').val() && $('#dimensions-width').val() && $('#dimensions-height').val()){
+          var CBM = ($('#dimensions-length').val() * $('#dimensions-width').val() * $('#dimensions-height').val()) / cm_divider;
+          var CBF = ($('#dimensions-length').val() * $('#dimensions-width').val() * $('#dimensions-height').val()) / cf_divider;
+          
+          td_costv1 = CBF*30;
+          td_costv1 = (td_costv1 < 90) ? 90 : td_costv1;
+          intcustv1 = td_costv1;
+          homedeliveryv1 = CBF*2.5;
+		
+          $("#form-cbm-v1").html(parseFloat(CBM).toFixed(2));
+          $("#form-cbf-v1").html(parseFloat(CBF).toFixed(2));
+	
+          $('.note__about-cost-v1').show();
+
+          $("#total-cost-v1").text(formatter.format(td_costv1));
+          $('.intcustom-v1').text("$"+parseFloat(intcustv1).toFixed(2));
+	  
+          $('.chk-home-delivery-val-v1').text("$"+parseFloat(homedeliveryv1).toFixed(2));
+
+          if ($('[name="home-delivery-v1"]').is(':checked')) {
+            $("#total-cost-v1").html(formatter.format(parseFloat(td_costv1)+parseFloat(shipprotectionv1)+parseFloat(homedeliveryv1)));
+          }else{
+             $("#total-cost-v1").html(formatter.format(parseFloat(td_costv1)+parseFloat(shipprotectionv1)));
+          }
+        }else{
+            $('.note__about-cost-v1').hide();
+        }
+      });
+    
+    $(function()
+    {
+      $('[name="check-shipment-v1"]').change(function()
+      {
+        shipprotectionv1 = 0;
+        if ($(this).is(':checked')) {
+          $('.shipment-div-v1').show();
+              }else{
+          $('.shipment-div-v1').hide();
+          $('.chk-shipment-protection-val-v1').text('');
+          $('.shipping-amount-v1').val('');
+
+          if($('[name="home-delivery-v1"]').is(':checked')) {
+            $("#total-cost-v1").html(formatter.format(parseFloat(shipprotectionv1+td_costv1+homedeliveryv1)));
+          }else{
+            $("#total-cost-v1").html(formatter.format(parseFloat(td_costv1+shipprotectionv1)));
+          }
+        };
+      });
+    });
+    
+    $('.chk-home-delivery-val-v1').hide();
+	  
+    $(function(){
+	  $('[name="home-delivery-v1"]').change(function(){
+	    if ($(this).is(':checked')) {
+	       $('.chk-home-delivery-val-v1').show();
+	       $("#total-cost-v1").html(formatter.format(parseFloat(td_costv1)+parseFloat(shipprotectionv1)+homedeliveryv1));
+	    }else{
+		$('.chk-home-delivery-val-v1').hide();
+		$("#total-cost-v1").html(formatter.format(parseFloat(td_costv1)+parseFloat(shipprotectionv1)));
+	    }
+	  });
+     });
+    
+  $('.shipping-amount-v1').on('input', function() {
+	  shipprotectionv1 = ($(this).val() < 100) ? 0 : ($(this).val()*5)/100;
+	   $('.chk-shipment-protection-val-v1').text("$"+formatter.format(parseFloat(shipprotectionv1)));
+
+	  if($('[name="home-delivery-v1"]').is(':checked')) {
+	   $("#total-cost-v1").html(formatter.format(parseFloat(shipprotectionv1)+homedeliveryv1+parseFloat(td_costv1)));
+	  }else{
+	    $("#total-cost-v1").html(formatter.format(parseFloat(shipprotectionv1+td_costv1)));
+	  }
+    });
+    
+    $(function()
+    {
+      $('[name="check-shipment"]').change(function()
+      {
+        shipprotection = 0;
+        if ($(this).is(':checked')) {
+           $('.shipment-div').show();
+        }else{
+		$('.shipment-div').hide();
+		$('.chk-shipment-protection-val').text('');
+		$('.shipping-amount').val('');
+
+		if($('[name="home-delivery"]').is(':checked')) {
+		   $("#total-cost-1").html(formatter.format(parseFloat(shipprotection+td_cost+homedelivery)));
+		}else{
+		   $("#total-cost-1").html(formatter.format(parseFloat(td_cost+shipprotection)));
+		}
+        };
+      });
+    });
+    $('.chk-home-delivery-val').hide();
+    
+   $(function(){
+      $('[name="home-delivery"]').change(function(){
+        if ($(this).is(':checked')) {
+           $('.chk-home-delivery-val').show();
+           $("#total-cost-1").html(formatter.format(parseFloat(td_cost)+parseFloat(shipprotection)+homedelivery));
+        }else{
+       	   $('.chk-home-delivery-val').hide();
+           $("#total-cost-1").html(formatter.format(parseFloat(td_cost)+parseFloat(shipprotection)));
+        }
+      });
+    });
+    
+  $('.shipping-amount').on('input', function() {
+    	shipprotection = ($(this).val() < 100) ? 0 : ($(this).val()*5)/100;
+	  
+       $('.chk-shipment-protection-val').text("$"+formatter.format(parseFloat(shipprotection)));
+	
+	if($('[name="home-delivery"]').is(':checked')) {
+	   $("#total-cost-1").html(formatter.format(parseFloat(shipprotection+homedelivery+td_cost)));
+	}else{
+	   $("#total-cost-1").html(formatter.format(parseFloat(shipprotection+td_cost)));
+	}
+    });
+    
+      $('.cost-calc-1 input').on('input', function() {
+        $('#dimensions-length-2').val() ? $('#dimensions-length-2').css({'border-color': '#737373'}) : $('#dimensions-length-2').css({'border-color': 'red'});
+      	$('#dimensions-width-2').val() ? $('#dimensions-width-2').css({'border-color': '#737373'}) : $('#dimensions-width-2').css({'border-color': 'red'});
+      	$('#dimensions-height-2').val() ? $('#dimensions-height-2').css({'border-color': '#737373'}) : $('#dimensions-height-2').css({'border-color': 'red'});
+				
+        if($('#dimensions-length-2').val() && $('#dimensions-width-2').val() && $('#dimensions-height-2').val()){
+          
+	  var CBM = ($('#dimensions-length-2').val() * $('#dimensions-width-2').val() * $('#dimensions-height-2').val()) / cm_divider;
+          var CBF = ($('#dimensions-length-2').val() * $('#dimensions-width-2').val() * $('#dimensions-height-2').val()) / cf_divider;
+          
+	  td_cost = CBF*30;
+	  td_cost = (td_cost < 90) ? 90 : td_cost;
+	  intcust = td_cost;
+	  homedelivery = CBF*2.5;
+		
+          $("#form-cbm").html(parseFloat(CBM).toFixed(2));
+          $("#form-cbf").html(parseFloat(CBF).toFixed(2));
+	
+          $('.note__about-cost').show();
+	
+	  $("#total-cost-1").text(formatter.format(td_cost));
+         
+          $('.intcustom').text("$"+parseFloat(intcust).toFixed(2));
+	
+          $('.chk-home-delivery-val').text("$"+parseFloat(CBF*2.5).toFixed(2));
+          
+          if ($('[name="home-delivery"]').is(':checked')) {
+           $("#total-cost-1").html(formatter.format(parseFloat(td_cost)+parseFloat(shipprotection)+parseFloat(homedelivery)));
+          }else{
+             $("#total-cost-1").html(formatter.format(parseFloat(td_cost)+parseFloat(shipprotection)));
+          }
+		
+        }else{
+            $('.note__about-cost-v1').hide();
+        }
+        
+      });
+  });
